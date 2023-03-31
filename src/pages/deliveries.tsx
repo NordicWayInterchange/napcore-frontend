2a3395bbf0e3927b2fb53457be6be664ce4b68a5
Tom-Anders Røst <tom.anders.roest@gmail.com>
2023-03-31 11:01:10 +0200

    fixed import

--- src/pages/deliveries.tsx
+++ src/pages/deliveries.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
-import { useDeliveries } from "../hooks/useDeliveries";
+import { useDeliveries } from "@/hooks/useDeliveries";
 
 const deliveries = () => {
   const { data, isLoading, isFetching } = useDeliveries("anna");
