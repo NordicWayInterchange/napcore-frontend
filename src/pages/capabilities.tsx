91d50ecb8bd18fa319e5b5f09495e6a5f63bcc5f
Tom-Anders Røst <tom.anders.roest@gmail.com>
2023-03-31 11:22:49 +0200

    Adds capability page

---
+++ src/pages/capabilities.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import { useCapabilities } from "../hooks/useCapabilities";
+
+const Capabilities = () => {
+  const { data, isLoading, isFetching } = useCapabilities("anna");
+  return (
+    <Box>
+      <Typography variant="h4">Capabilities</Typography>
+    </Box>
+  );
+};
+
+export default Capabilities;
