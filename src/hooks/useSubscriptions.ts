b8b1985145eebc904f24eea9678ec2b64c72b26a
Tom-Anders Røst <tom.anders.roest@gmail.com>
2023-03-31 10:15:25 +0200

    rewrote to async function

--- src/hooks/useSubscriptions.ts
+++ src/hooks/useSubscriptions.ts
@@ -1,13 +1,14 @@
 import { Subscriptions } from "@/types/subscription";
 import { useQuery } from "@tanstack/react-query";
 
-const fetchSubscriptions: (userName: string) => Promise<Subscriptions> = (
+const fetchSubscriptions: (userName: string) => Promise<Subscriptions> = async (
   userName: string
 ) => {
-  return fetch("http://localhost:3000/api/mock-subscriptions").then((res) => {
-    const data = res.json();
-    return data;
-  });
+  const res = await fetch(
+    `http://localhost:3000/api/napcore/${userName}/subscriptions`
+  );
+
+  return res.json();
 };
 
 const useSubscriptions = (userName: string) => {
