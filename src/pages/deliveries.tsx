db2ffae9d289a194e2981909301030385c50e72c
Tom-Anders Røst <tom.anders.roest@gmail.com>
2023-03-31 10:17:11 +0200

    Adds fetching of data for the different pages

--- src/pages/deliveries.tsx
+++ src/pages/deliveries.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
+import { useDeliveries } from "../hooks/useDeliveries";
 
 const deliveries = () => {
+  const { data, isLoading, isFetching } = useDeliveries("anna");
   return (
     <Box>
-      <Typography variant="h4">Network Capabilities</Typography>
+      <Typography variant="h4">Deliveries</Typography>
     </Box>
   );
 };
--- src/pages/subscriptions.tsx
+++ src/pages/subscriptions.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
 import TableContainer from "@/components/table/TableContainer";
+import { useSubscriptions } from "../hooks/useSubscriptions";
 
 const subscriptions = () => {
+  const { data, isLoading, isFetching } = useSubscriptions("anna");
   return (
     <Box>
       <Typography variant="h4">Subscription</Typography>
