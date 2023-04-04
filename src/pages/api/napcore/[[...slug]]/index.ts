c778c60a188c6cd07544c5d76280602027dae1af
Tom-Anders Røst <tom.anders.roest@gmail.com>
2023-04-04 12:21:01 +0200

    Adds aggregate and counter endpoints, and support for selector queryparam (#26)

--- src/pages/api/napcore/[[...slug]]/index.ts
+++ src/pages/api/napcore/[[...slug]]/index.ts
@@ -1,44 +1,112 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
-import { Capabilities } from "@/types/capability";
+import { Capabilities, Capability, LocalCapability } from "@/types/capability";
 import { getTLSAgent } from "@/lib/sslAgent";
 
 const headers = {
   Accept: "application/json",
 };
+
 const fetchIXN: (
   userName: string,
-  path: string
-) => Promise<Capabilities> = async (userName, path) => {
+  path: string,
+  selector?: string
+) => Promise<any> = async (userName, path, selector = "") => {
   const uri = process.env.INTERCHANGE_URI || "";
   const uriPath = `${userName}${path}`;
   const agent = getTLSAgent();
-  const respons = await axios(uri + uriPath, { headers, httpsAgent: agent });
+  const params: { selector?: string } = {};
+  if (selector) {
+    params.selector = selector;
+  }
+  const respons = await axios(uri + uriPath, {
+    params,
+    headers,
+    httpsAgent: agent,
+  });
 
   return await respons.data;
 };
 
-const fetchNetworkCapabilities = (userName: string) => {
-  return fetchIXN(userName, "/network/capabilities");
+const fetchNetworkCapabilities = async (
+  userName: string,
+  selector: string = ""
+) => {
+  const capabilities: Capabilities = await fetchIXN(
+    userName,
+    "/network/capabilities",
+    selector
+  );
+  return capabilities.capabilities.map((capability) => {
+    return capability.definition;
+  });
 };
 
-const fetchCapabilities = (userName: string) => {
-  return fetchIXN(userName, "/capabilities");
+const fetchCapabilities = async (userName: string, selector: string = "") => {
+  const capabilities: Capabilities = await fetchIXN(
+    userName,
+    "/capabilities",
+    selector
+  );
+  return capabilities.capabilities.map((capability) => {
+    return capability.definition;
+  });
 };
 
-const fetchSubscriptions = (userName: string) => {
-  return fetchIXN(userName, "/subscriptions");
+const fetchSubscriptions = (userName: string, selector: string = "") => {
+  return fetchIXN(userName, "/subscriptions", selector);
 };
 
-const fetchDeliveries = (userName: string) => {
-  return fetchIXN(userName, "/deliveries");
+const fetchDeliveries = (userName: string, selector: string = "") => {
+  return fetchIXN(userName, "/deliveries", selector);
 };
 
-const getPaths: { [key: string]: (userName: string) => Promise<any> } = {
+const fetchCapabilityCounter = async (userName: string, selector?: string) => {
+  const capabilities = await fetchNetworkCapabilities(userName, selector);
+  return capabilities.length;
+};
+
+const fetchAggregate = async (userName: string, selector?: string) => {
+  const capabilities = await fetchNetworkCapabilities(userName, selector);
+
+  return capabilities.reduce(
+    (acc: { [key: string]: any }, capability: Capability) => {
+      (Object.keys(capability) as Array<keyof typeof capability>).forEach(
+        (capabilityProp) => {
+          let value = Array.isArray(capability[capabilityProp])
+            ? (capability[capabilityProp] as Array<string>)
+            : [capability[capabilityProp] as string];
+          if (capabilityProp in acc) {
+            // get only new values
+            value = value.filter((val) => !acc[capabilityProp].includes(val));
+            acc[capabilityProp] = [...acc[capabilityProp], ...value];
+          } else {
+            acc[capabilityProp] = value;
+          }
+        }
+      );
+      return acc;
+    },
+    {}
+  );
+};
+
+// all getter methods on path
+const getPaths: {
+  [key: string]: (userName: string, selector?: string) => Promise<any>;
+} = {
   "network/capabilities": fetchNetworkCapabilities,
   capabilities: fetchCapabilities,
   subscriptions: fetchSubscriptions,
   deliveries: fetchDeliveries,
+};
+
+// all internal fetchers
+const getInternal: {
+  [key: string]: (userName: string, selector?: string) => any;
+} = {
+  aggregate: fetchAggregate,
+  ["capability-count"]: fetchCapabilityCounter,
 };
 
 export default async function handler(
@@ -49,6 +117,9 @@ export default async function handler(
     ? req.query.slug
     : [req.query.slug];
 
+  const selector = Array.isArray(req.query.selector)
+    ? req.query.selector[0]
+    : req.query.selector;
   const [actorCommonName, ...path] = slug;
   const urlPath = path.join("/");
 
@@ -57,7 +128,7 @@ export default async function handler(
 
     switch (method) {
       case "GET":
-        const data = await getPaths[urlPath](actorCommonName);
+        const data = await getPaths[urlPath](actorCommonName, selector);
         res.status(200).json(data);
         break;
       case "POST":
@@ -69,6 +140,14 @@ export default async function handler(
         break;
     }
     return;
+  }
+  if (actorCommonName && [...Object.keys(getInternal)].includes(urlPath)) {
+    try {
+      const data = await getInternal[urlPath](actorCommonName, selector);
+      return res.status(200).json(data);
+    } catch (err) {
+      return res.status(err.response.status).json(err.data);
+    }
   }
   return res.status(404).json({ description: `Page not found: ${urlPath}` });
 }
