104a38bc43790ee78dbec7b7f83614aae8c4323e
Henrik Bråthen <henrik.brathen@bouvet.no>
2023-04-14 14:42:00 +0200

    selector for quadtree

---
+++ __tests__/generateSelector.test.ts
@@ -0,0 +1,22 @@
+// expect(hello('world')).toBe('Hello world')
+
+import { generateSelector } from "@/lib/generateSelector";
+
+describe("Generate Selector", () => {
+  const subscription = {
+    messageType: "DENM, IVIM",
+    protocolVersion: "DENM:1.1.1",
+    originatingCountry: "NO, SE",
+    publisherId: "Bouvet-1, Bouvet-2",
+    quadTree: ["1", "2", "3", "123"],
+  };
+  it("can generate select", () => {
+    const selector =
+      "((messageType = 'DENM') OR (messageType = ' IVIM')) AND " +
+      "(protocolVersion = 'DENM:1.1.1') AND " +
+      "((originatingCountry = 'NO') OR (originatingCountry = ' SE')) AND " +
+      "((publisherId = 'Bouvet-1') OR (publisherId = ' Bouvet-2')) AND " +
+      "(quadTree like '%,1%' OR quadTree like '%,2%' OR quadTree like '%,3%' OR quadTree like '%,123%')";
+    expect(generateSelector(subscription)).toBe(selector);
+  });
+});
--- src/lib/generateSelector.ts
+++ src/lib/generateSelector.ts
@@ -1,49 +1,33 @@
 import { Subscription } from "@/types/subscription";
 
 /* TODO
- *   - subValue can be a number (timestamp)
- *   - quadtree reducer
- *   - leading spaces after comma, without any letters (eg. DENM:1.1.2,    )
  *   - Type for generateSelectorParameter
- *   - Write comments
  * */
 
-export const generateSelector = (subscription: any): string => {
-  return Object.keys(subscription).reduce(
+export const generateSelector = (formState: any): string => {
+  return Object.keys(formState).reduce(
     (acc: string, key: string, index: number) => {
-      const subValue = subscription[key as keyof typeof subscription];
-      const subValue2 = Array.isArray(subValue)
-        ? subValue
-        : subValue.trim().split(",").filter(Boolean);
+      const value = formState[key as keyof typeof formState];
+      const valueArray = Array.isArray(value)
+        ? value
+        : value.trim().split(",").filter(Boolean);
 
-      if (subValue2.length > 0) {
+      if (valueArray.length > 0) {
         if (index && acc) {
           acc += " AND ";
         }
-        const reduce = subValue2.reduce(
+        const reduce = valueArray.reduce(
           (acc: string, value: string, index: number) =>
             reducer(acc, value, index, key),
           ""
         );
-        2;
-        acc += subValue2.length > 1 ? `(${reduce})` : reduce;
+        acc += valueArray.length > 1 ? `(${reduce})` : reduce;
       }
 
       return acc;
     },
     ""
   );
-};
-
-const quadTreeReducer = (quadTree: string[]) => {
-  quadTree.reduce((acc, value, index) => {
-    if (index != 0) {
-      acc += " OR ";
-    }
-    acc += `quadTree like '%,${value}%'`;
-    // TODO () rundt acc
-    return acc;
-  }, "");
 };
 
 const reducer = (
@@ -56,7 +40,13 @@ const reducer = (
     if (index != 0) {
       acc += " OR ";
     }
-    acc += `(${key} = '${value}')`;
+    switch (key) {
+      case "quadTree":
+        acc += `${key} like '%,${value}%'`;
+        break;
+      default:
+        acc += `(${key} = '${value}')`;
+    }
   }
   return acc;
 };
