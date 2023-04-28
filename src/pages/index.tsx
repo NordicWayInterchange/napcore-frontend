cc17f806a98a640c4df3c180ae0103f3740a9782
Henrik Bråthen <henrik.brathen@bouvet.no>
2023-04-28 16:59:23 +0200

    load map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 import Subscriptions from "./subscriptions";
 
 export default function Home() {
-  return <Subscriptions />;
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../components/MapView"), {
+        loading: () => <p>A map is loading</p>,
+        ssr: false,
+      }),
+    [
+      /* list variables which should trigger a re-render */
+    ]
+  );
+  return <Map />;
 }
