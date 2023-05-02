3f88ee5142d9e82b9c9cad210df792082a12f4e8
Henrik Bråthen <henrik.brathen@bouvet.no>
2023-05-02 12:56:07 +0200

    map init

--- src/components/leaflet/MapView.tsx
+++ src/components/leaflet/MapView.tsx
@@ -2,23 +2,25 @@ import React from "react";
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+const heightAndWidth = 900;
+
 export default function MapView() {
   return (
     <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
-      scrollWheelZoom={false}
-      style={{ height: 700, width: 700 }}
+      center={[51.5072, 0.1275]}
+      zoom={4}
+      scrollWheelZoom={true}
+      style={{ height: heightAndWidth, width: heightAndWidth }}
     >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      {/* <Marker position={[51.505, -0.09]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
-      </Marker>
+      </Marker> */}
     </MapContainer>
   );
 }
