21978c2d78c61da7d3a65b216a7f944456863e4e
Henrik Bråthen <henrik.brathen@bouvet.no>
2023-06-01 12:32:56 +0200

    list in the sub. drawer

--- src/components/subscriptions/SubscriptionsDrawer.tsx
+++ src/components/subscriptions/SubscriptionsDrawer.tsx
@@ -67,117 +67,149 @@ const SubscriptionsDrawer = ({
       onClose={handleClose}
     >
       <Toolbar />
-      <Box sx={{ padding: 2 }}>
-        {/*        <IconButton onClick={handleMoreClose} edge="end" color="primary">
+      <Box sx={{ padding: 1, width: 1 }}>
+        <List>
+          {/*        <IconButton onClick={handleMoreClose} edge="end" color="primary">
           <CloseIcon />
         </IconButton>*/}
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <Typography>Subscription details</Typography>
-          <Chip
-            sx={{ borderRadius: 1 }}
-            // TODO: Fix
-            color={statusChips[subscription.status]}
-            label={subscription.status}
-          />
-        </Box>
-        <Box
-          sx={{
-            backgroundColor: theme.palette.mainBackgroundColor,
-            padding: 1,
-            borderRadius: 2,
-          }}
-        >
-          <Box>{subscription.id}</Box>
-          <Box>{subscription.capabilityMatches}</Box>
-          <Box>{subscription.lastUpdatedTimeStamp}</Box>
-        </Box>
-        <Box
-          sx={{
-            backgroundColor: theme.palette.mainBackgroundColor,
-            padding: 1,
-            borderRadius: 2,
-          }}
-        >
-          {/*TODO: add enpoints (currently existing)*/}
-          {/*<Box>{subscription.endpoints[0].host}</Box>
+          <ListItem>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                width: 1,
+              }}
+            >
+              <Typography>Subscription details</Typography>
+              <Chip
+                sx={{ borderRadius: 1 }}
+                // TODO: Fix
+                color={statusChips[subscription.status]}
+                label={subscription.status}
+              />
+            </Box>
+          </ListItem>
+          <ListItem>
+            <Box
+              sx={{
+                backgroundColor: theme.palette.mainBackgroundColor,
+                padding: 2,
+                borderRadius: 2,
+                width: 1,
+              }}
+            >
+              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Box>
+                  <ListItemText primary={"ID"} secondary={subscription.id} />
+                  <ListItemText
+                    primary={"Capability matches"}
+                    secondary={subscription.capabilityMatches}
+                  />
+                </Box>
+                <ListItemText
+                  primary={"Last updated"}
+                  // TODO: Update
+                  secondary={"12:46"}
+                />
+              </Box>
+            </Box>
+          </ListItem>
+          <ListItem>
+            <Box
+              sx={{
+                backgroundColor: theme.palette.mainBackgroundColor,
+                padding: 2,
+                borderRadius: 2,
+                width: 1,
+              }}
+            >
+              {/*TODO: add enpoints (currently existing)*/}
+              {/*<Box>{subscription.endpoints[0].host}</Box>
           <Box>{subscription.endpoints[0].source}</Box>
           <Box>{subscription.endpoints[0].port}</Box>*/}
-          <Typography>ENDPOINTS</Typography>
-          <FormControl fullWidth>
-            <TextField
-              contentEditable={false}
-              value={"ampqs://myserver"}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={() => copyPrivateKey("ampqs://myserver")}
-                      edge="end"
-                      color="primary"
-                    >
-                      <ContentCopyIcon />
-                    </IconButton>
-                  </InputAdornment>
-                ),
+              <Typography>ENDPOINTS</Typography>
+              <FormControl fullWidth>
+                <TextField
+                  contentEditable={false}
+                  value={"ampqs://myserver"}
+                  label={"Host"}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          onClick={() => copyPrivateKey("ampqs://myserver")}
+                          edge="end"
+                          color="primary"
+                        >
+                          <ContentCopyIcon />
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <TextField
+                  contentEditable={false}
+                  value={"Serviceprovider 1-1"}
+                  label={"Source"}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          onClick={() => copyPrivateKey("Serviceprovider 1-1")}
+                          edge="end"
+                          color="primary"
+                        >
+                          <ContentCopyIcon />
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <TextField
+                  contentEditable={false}
+                  value={"5671"}
+                  label={"Port"}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          onClick={() => copyPrivateKey("5671")}
+                          edge="end"
+                          color="primary"
+                        >
+                          <ContentCopyIcon />
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </FormControl>
+            </Box>
+          </ListItem>
+
+          <ListItem>
+            <Box
+              sx={{
+                backgroundColor: theme.palette.mainBackgroundColor,
+                padding: 2,
+                borderRadius: 2,
+                width: 1,
               }}
-            />
-            <TextField
-              contentEditable={false}
-              value={"Serviceprovider 1-1"}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={() => copyPrivateKey("Serviceprovider 1-1")}
-                      edge="end"
-                      color="primary"
-                    >
-                      <ContentCopyIcon />
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <TextField
-              contentEditable={false}
-              value={"5671"}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={() => copyPrivateKey("5671")}
-                      edge="end"
-                      color="primary"
-                    >
-                      <ContentCopyIcon />
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </FormControl>
-        </Box>
-        <Box
-          sx={{
-            backgroundColor: theme.palette.mainBackgroundColor,
-            padding: 1,
-            borderRadius: 2,
-          }}
-        >
-          <Typography>SELECTOR</Typography>
-          <FormControl fullWidth>
-            <TextField value={subscription.selector} rows={4} />
-          </FormControl>
-        </Box>
-        <Button sx={{ borderRadius: 100 }} variant={"contained"}>
-          Remove Subscription
-        </Button>
+            >
+              <Typography>SELECTOR</Typography>
+              <FormControl fullWidth>
+                <TextField multiline value={subscription.selector} rows={4} />
+              </FormControl>
+            </Box>
+          </ListItem>
+
+          <ListItem>
+            <Button sx={{ borderRadius: 100 }} variant={"contained"}>
+              Remove Subscription
+            </Button>
+          </ListItem>
+        </List>
       </Box>
     </Drawer>
   );
