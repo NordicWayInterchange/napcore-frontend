d8bacb37080463451ee35131c5d4cc8321c17798
Henrik Bråthen <henrik.brathen@bouvet.no>
2023-07-05 15:49:40 +0200

    extract colors

--- src/theme/trafficdata.ts
+++ src/theme/trafficdata.ts
@@ -1,43 +1,12 @@
 import { createTheme } from "@mui/material";
 import { openSans } from "@/theme/fonts";
-const { palette } = createTheme();
-const { augmentColor } = palette;
-const createColor = (mainColor: any) =>
-  augmentColor({ color: { main: mainColor } });
+import { TRAFFICDATA_COLORS } from "@/theme/colors";
 
 export const trafficdata = createTheme({
-  /*FONT*/
   typography: {
     fontFamily: `${openSans.style.fontFamily}, sans-serif`,
   },
-
-  /*PALETTE*/
   palette: {
-    navbarBackgroundColor: "#d12e2c",
-    mainBackgroundColor: "#f0f1f1",
-
-    sidebarActiveColor: "#E5E5E5",
-
-    avatarBackgroundColor: "#d12e2c",
-    buttonThemeColor: createColor("#d12e2c"),
-
-    cardBackgroundColor: "#E5E5E5",
-
-    // buttons and fonts
-    depricatedRed: createColor("#F8DEDE"),
-
-    // chip colors
-    grayDark: createColor("#2E3539"),
-    grayLight: createColor("#E5E5E5"),
-    depricatedDark: createColor("#7E1010"),
-    depricatedLight: createColor("#F8DEDE"),
-    greenDark: createColor("#3E7B1F"),
-    greenLight: createColor("#EBF5E6"),
-    blueDark: createColor("#006C9A"),
-    blueLight: createColor("#D4F7FF"),
-    yellowDark: createColor("#A17E00"),
-    yellowLight: createColor("#FFF5C8"),
-    pinkDark: createColor("#9C176F"),
-    pinkLight: createColor("#EDCEF5"),
+    ...TRAFFICDATA_COLORS,
   },
 });
