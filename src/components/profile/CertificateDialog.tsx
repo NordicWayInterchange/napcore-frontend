d1c28da3c87024bb0a43ce4f7219e1f153bf6ab3
Henrik Bråthen <henrik.brathen@bouvet.no>
2023-07-07 09:35:12 +0200

    tmp

--- src/components/profile/CertificateDialog.tsx
+++ src/components/profile/CertificateDialog.tsx
@@ -6,13 +6,12 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
-  FormControl,
-  IconButton,
-  InputAdornment,
-  TextField,
+  Divider,
 } from "@mui/material";
-import DownloadIcon from "@mui/icons-material/Download";
 import { CertificateSignResponse } from "@/types/napcore/certificate";
+import { useSession } from "next-auth/react";
+import { handleDecoding, handleFile } from "@/lib/handleFile";
+import { Box } from "@mui/system";
 
 type Props = {
   privateKey: string;
@@ -21,58 +20,109 @@ type Props = {
   handleDialog: (close: boolean) => void;
 };
 
+const commonNamePrefix = process.env.NEXT_PUBLIC_INTERCHANGE_PREFIX;
+
 export default function CertificateDialog(props: Props) {
   const { privateKey, open, handleDialog, chain } = props;
+  const { data: session } = useSession();
 
   const handleClose = () => {
     handleDialog(false);
   };
 
-  /*TODO: Theme colors*/
+  const downloadButtons = [
+    {
+      text: "Download private key",
+      onClick: () => handleFile(privateKey, "privateKey.txt"),
+    },
+    {
+      text: "Download chain certificate",
+      onClick: () =>
+        handleFile(
+          handleDecoding(chain.chain),
+          `chain.${
+            ((commonNamePrefix as string) + session?.user?.email) as string
+          }.txt`
+        ),
+    },
+    {
+      text: "Download root certificate",
+      onClick: () =>
+        handleFile(handleDecoding(chain.chain.at(-1) as string), "root.txt"),
+    },
+  ];
 
   return (
     <>
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Certificate generated</DialogTitle>
+        <DialogTitle>Certificate was successfully created</DialogTitle>
         <DialogContent>
           <DialogContentText sx={{ marginY: 1 }}>
-            The certificate was successfully created.
+            Please download your private key, chain certificate and root
+            certificate before closing this window.
           </DialogContentText>
-          <DialogContentText sx={{ marginY: 1 }}>
-            Please download your private key and .cert file before closing this
-            window.
-          </DialogContentText>
-          <FormControl fullWidth>
-            <TextField
-              contentEditable={false}
-              value={privateKey}
-              margin="normal"
-              label={"Private key"}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      edge="end"
-                      href={`data:text/json;charset=utf-8,${encodeURIComponent(
-                        privateKey
-                      )}`}
-                      download={"privateKey.txt"}
-                    >
-                      <DownloadIcon />
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </FormControl>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Button
+              variant="text"
+              color="greenDark"
+              sx={{ borderRadius: 100, textTransform: "none", width: 300 }}
+              onClick={() =>
+                /*handleFile(privateKey, "privateKey.key.pem"*/
+                handleFile(privateKey, "privateKey.txt")
+              }
+              disableElevation
+            >
+              Download private key
+            </Button>
+            <Button
+              variant="text"
+              color="greenDark"
+              sx={{ borderRadius: 100, textTransform: "none", width: 300 }}
+              onClick={() =>
+                /*handleFile(
+                  chain,
+                  `chain.${
+                    ((commonNamePrefix as string) +
+                      session?.user?.email) as string
+                  }.crt.pem`
+                )*/
+                handleFile(
+                  handleDecoding(chain.chain),
+                  `chain.${
+                    ((commonNamePrefix as string) +
+                      session?.user?.email) as string
+                  }.txt`
+                )
+              }
+              disableElevation
+            >
+              Download chain certificate
+            </Button>
+            <Button
+              variant="text"
+              color="greenDark"
+              sx={{ borderRadius: 100, textTransform: "none", width: 300 }}
+              onClick={() =>
+                /*handleFile(chain.certificates.at(-1), "root.crt.pem")*/
+                handleFile(
+                  handleDecoding(chain.chain.at(-1) as string),
+                  "root.txt"
+                )
+              }
+              disableElevation
+            >
+              Download root certificate
+            </Button>
+          </Box>
         </DialogContent>
-        <DialogActions
-          sx={{
-            display: "flex",
-            justifyContent: "space-evenly",
-            marginBottom: 2,
-          }}
-        >
+        <Divider />
+        <DialogActions>
           <Button
             variant="text"
             color="greenDark"
@@ -81,19 +131,6 @@ export default function CertificateDialog(props: Props) {
             disableElevation
           >
             Close
-          </Button>
-          <Button
-            startIcon={<DownloadIcon />}
-            variant="contained"
-            color="greenDark"
-            sx={{ borderRadius: 100, textTransform: "none" }}
-            href={`data:text/json;charset=utf-8,${encodeURIComponent(
-              JSON.stringify(chain)
-            )}`}
-            download={"encodedCertChain.json"}
-            disableElevation
-          >
-            Download certificate
           </Button>
         </DialogActions>
       </Dialog>
