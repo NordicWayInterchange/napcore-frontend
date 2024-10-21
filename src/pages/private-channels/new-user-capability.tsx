1ddc6f584a47570327882dc6c1314907a2be7024
Maryam Mehrabian <marme660@users.noreply.github.com>
2024-10-21 12:48:23 +0200

    Added private channel form and navigation

---
+++ src/pages/private-channels/new-user-capability.tsx
@@ -0,0 +1,27 @@
+import { Divider, Grid } from "@mui/material";
+import React from "react";
+import { Box } from "@mui/system";
+import Subheading from "@/components/shared/display/typography/Subheading";
+import Mainheading from "@/components/shared/display/typography/Mainheading";
+import UserCapabilitiesSelectorBuilder from "@/components/userCapabilities/UserCapabilitiesSelectorBuilder";
+import { BreadcrumbNavigation } from "@/components/shared/actions/BreadcrumbNavigation";
+const NewUserCapability = () => {
+
+  return (
+    <Box flex={1}>
+      <Mainheading>Add my capability</Mainheading>
+      <Subheading>
+        Add a user capability with the form.
+      </Subheading>
+      <Divider sx={{ marginY: 1 }} />
+      <BreadcrumbNavigation text="My capabilities" />
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
+          <UserCapabilitiesSelectorBuilder />
+        </Grid>
+        </Grid>
+    </Box>
+  );
+};
+
+export default NewUserCapability;
