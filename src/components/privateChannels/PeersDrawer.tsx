2796f28d4d8eec5cd74b8311ac0e6f5009bc2b75
Maryam Mehrabian <marme660@users.noreply.github.com>
2024-10-25 12:08:43 +0200

    Added peers

--- src/components/privateChannels/PeersDrawer.tsx
+++ src/components/privateChannels/PeersDrawer.tsx
@@ -8,10 +8,9 @@ import {
 } from "@mui/material";
 import React, { useState } from "react";
 import { useSession } from "next-auth/react";
-import DeleteSubDialog from "@/components/shared/actions/DeleteSubDialog";
 import { IFeedback } from "@/interface/IFeedback";
 import Snackbar from "@/components/shared/feedback/Snackbar";
-import { PrivateChannel } from "@/types/napcore/privateChannel";
+import { PrivateChannelPeers } from "@/types/napcore/privateChannel";
 import CloseIcon from "@mui/icons-material/Close";
 import { Chip } from "@/components/shared/display/Chip";
 import { statusChips } from "@/lib/statusChips";
@@ -21,15 +20,13 @@ import { styled } from "@mui/material/styles";
 const width = 600;
 
 type Props = {
-  privateChannel: PrivateChannel;
+  peers: PrivateChannelPeers;
   open: boolean;
   handleMoreClose: () => void;
-  handleDeletedItem: (deleted: boolean) => void;
 };
 
-const PrivateChannelsDrawer = ({ privateChannel, open, handleMoreClose, handleDeletedItem }: Props) => {
+const PeersDrawer = ({ peers, open, handleMoreClose }: Props) => {
   const { data: session } = useSession();
-  const [openMap, setOpenMap] = useState<boolean>(false);
   const [dialogOpen, setDialogOpen] = useState<boolean>(false);
   const [feedback, setFeedback] = useState<IFeedback>({
     feedback: false,
@@ -48,9 +45,6 @@ const PrivateChannelsDrawer = ({ privateChannel, open, handleMoreClose, handleDe
     setFeedback({ feedback: false, message: "", severity: "success" });
   };
 
-  const handleClose = () => {
-    setOpenMap(false);
-  };
 
   const handleClickClose = (close: boolean) => {
     setDialogOpen(close);
@@ -88,14 +82,14 @@ const PrivateChannelsDrawer = ({ privateChannel, open, handleMoreClose, handleDe
             </ListItem>
             <ListItem>
               <StyledHeaderBox>
-                <Typography>Private channel details</Typography>
+                <Typography>Peers details</Typography>
                 <Chip
                   color={
                     statusChips[
-                      privateChannel.status.toString() as keyof typeof statusChips
+                      peers.status.toString() as keyof typeof statusChips
                       ] as any
                   }
-                  label={privateChannel.status}
+                  label={peers.status}
                 />
               </StyledHeaderBox>
             </ListItem>
@@ -105,12 +99,12 @@ const PrivateChannelsDrawer = ({ privateChannel, open, handleMoreClose, handleDe
                 <FormControl fullWidth>
                   <TextField
                     contentEditable={false}
-                    value={privateChannel.id}
+                    value={peers.id}
                     label={"ID"}
                     margin="normal"
                     InputProps={{
                       endAdornment: (
-                        <ContentCopy value={privateChannel.id} />
+                        <ContentCopy value={peers.id} />
                       )
                     }}
                   />
@@ -118,71 +112,39 @@ const PrivateChannelsDrawer = ({ privateChannel, open, handleMoreClose, handleDe
               </StyledCard>
             </ListItem>
 
-            {privateChannel.peers && (
-              <ListItem>
-                <StyledCard variant={"outlined"}>
-                  <Typography  sx={{ marginBottom: 2 }}>Peers</Typography>
-                  <FormControl fullWidth>
-                    <InputLabel>Peers</InputLabel>
-                    <Select
-                      MenuProps={{ PaperProps: { sx: { maxHeight: 200 } } }}
-                      label="Peers"
-                      multiple
-                      defaultValue={privateChannel.peers.map(
-                        (peer) => {
-                          return peer;
-                        }
-                      )}
-                    >
-                      {privateChannel.peers.map((peer, index) => {
-                        return (
-                          <StyledMenuItem
-                            disabled
-                            key={index}
-                            value={peer}
-                          >
-                            {peer}
-                          </StyledMenuItem>
-                        );
-                      })}
-                    </Select>
-                  </FormControl>
-                </StyledCard>
-              </ListItem>
-            )}
-            {privateChannel.endpoint && (
+            {peers.endpoint && (
               <ListItem>
                 <StyledCard variant={"outlined"}>
                   <Typography>Endpoint</Typography>
                   <FormControl fullWidth>
                     <TextField
                       contentEditable={false}
-                      value={privateChannel.endpoint.host}
+                      value={peers.endpoint.host}
                       label={"Host"}
                       margin="normal"
                       InputProps={{
                         endAdornment: (
-                          <ContentCopy value={privateChannel.endpoint.host} />
+                          <ContentCopy value={peers.endpoint.host} />
                         ),
                       }}
                     />
                     <TextField
                       contentEditable={false}
-                      value={privateChannel.endpoint.port}
+                      value={peers.endpoint.port}
                       label="Port"
                       margin="normal"
                       InputProps={{
-                        endAdornment: <ContentCopy value={privateChannel.endpoint.port.toString()} />,
+                        endAdornment: <ContentCopy value={peers.endpoint.port.toString()} />,
                       }}
                     />
                     <TextField
                       contentEditable={false}
-                      value={privateChannel.endpoint.queueName}
+                      value={peers.endpoint.queueName}
                       label="Queue name"
                       margin="normal"
                       InputProps={{
                         endAdornment: (
-                          <ContentCopy value={privateChannel.endpoint.queueName} />
+                          <ContentCopy value={peers.endpoint.queueName} />
                         ),
                       }}
                     />
@@ -190,40 +152,6 @@ const PrivateChannelsDrawer = ({ privateChannel, open, handleMoreClose, handleDe
                 </StyledCard>
               </ListItem>
             )}
-            <ListItem>
-              <StyledCard variant={"outlined"}>
-                <Typography>Description</Typography>
-                <FormControl fullWidth>
-                  <TextField
-                    contentEditable={false}
-                    margin="normal"
-                    multiline
-                    value={privateChannel.description}
-                    rows={4}
-                    InputProps={{
-                      readOnly: true,
-                      endAdornment: (
-                        <ContentCopy value={privateChannel.description} />
-                      ),
-                    }}
-                  />
-                </FormControl>
-              </StyledCard>
-            </ListItem>
-            <ListItem >
-              <Button
-                sx={{
-                  borderRadius: 100,
-                  textTransform: "none",
-                }}
-                variant={"contained"}
-                color={"redLight"}
-                onClick={() => setDialogOpen(true)}
-                disableElevation
-              >
-                Remove private channel
-              </Button>
-            </ListItem>
           </List>
         </Box>
       </Drawer>
@@ -235,14 +163,6 @@ const PrivateChannelsDrawer = ({ privateChannel, open, handleMoreClose, handleDe
           handleClose={handleSnackClose}
         />
       )}
-      <DeleteSubDialog
-        open={dialogOpen}
-        actorCommonName={session?.user.commonName as string}
-        itemId={privateChannel.id as string}
-        handleDialog={handleClickClose}
-        handleDeletedItem={handleDeletedItem}
-        text="Private channel"
-      />
     </>
   );
 };
@@ -260,22 +180,6 @@ const StyledHeaderBox = styled(Box)(({}) => ({
   width: "100%",
 }));
 
-const StyledMenuItem = styled(MenuItem)(({}) => ({
-  "&.MuiMenuItem-root": {
-    color: "black",
-    opacity: 1
-  },
-  "&.Mui-disabled": {
-    color: "black",
-    opacity: 1
-  },
-  "&.Mui-selected": {
-    backgroundColor: "white",
-    "&.Mui-focusVisible": {
-      background: "white"
-    }
-  }
-}));
 
 
-export default PrivateChannelsDrawer;
+export default PeersDrawer;
