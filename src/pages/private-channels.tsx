9a6e4984b76e32de5a492d168b930d5d67018e3e
Maryam Mehrabian <marme660@users.noreply.github.com>
2024-10-28 10:53:29 +0100

    Hide peers in case there is no any

--- src/pages/private-channels.tsx
+++ src/pages/private-channels.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box, Divider, IconButton, Paper, TableContainer } from "@mui/material";
+import { Box, Divider, IconButton} from "@mui/material";
 import Mainheading from "@/components/shared/display/typography/Mainheading";
 import Subheading from "@/components/shared/display/typography/Subheading";
 import { GridColDef } from "@mui/x-data-grid";
@@ -21,6 +21,7 @@ import { performRefetch } from "@/lib/performRefetch";
 import PeersDrawer from "@/components/privateChannels/PeersDrawer";
 import { usePeers } from "@/hooks/usePeers";
 import { timeConverter } from "@/lib/timeConverter";
+import DeleteSubDialog from "@/components/shared/actions/DeleteSubDialog";
 
 export default function PrivateChannels() {
 
@@ -32,7 +33,6 @@ export default function PrivateChannels() {
   const {
     data: peersData,
     isLoading: isPeersLoading,
-    remove: removePeers,
   } = usePeers(session?.user.commonName as string);
 
   const [privateChannelRow, setPrivateChannelRow] = useState<PrivateChannel>();
@@ -41,13 +41,27 @@ export default function PrivateChannels() {
   const [peersDrawerOpen, setPeersDrawerOpen] = useState<boolean>(false);
   const [isDeleted, setIsDeleted] = useState<boolean>(false);
   const [open, setOpen] = useState<boolean>(false);
+  const [shouldRefreshAfterDelete, setShouldRefreshAfterDelete] = useState<boolean>(false);
+
+  const hasPeersData =  peersData && peersData.length > 0;
 
   useEffect(() => {
     if (isDeleted) {
       performRefetch(refetch);
       setIsDeleted(false);
+      setShouldRefreshAfterDelete(true);
     }
   }, [isDeleted, refetch]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      performRefetch(refetch);
+    }, 30000);
+    setShouldRefreshAfterDelete(false);
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, [shouldRefreshAfterDelete, refetch]);
 
   const handleMore = (privateChannel: PrivateChannel) => {
     setPrivateChannelRow(privateChannel);
@@ -61,12 +75,14 @@ export default function PrivateChannels() {
 
   const handleMoreClose = () => {
     setDrawerOpen(false);
-    remove();
+  };
+
+  const handleClickClose = (close: boolean) => {
+    setOpen(close);
   };
 
   const handlePeersMoreClose = () => {
     setPeersDrawerOpen(false);
-    removePeers();
   };
 
   const handleDelete = (privateChannel: PrivateChannel) => {
@@ -197,8 +213,8 @@ export default function PrivateChannels() {
     <Box flex={1}>
       <Mainheading>Private channels</Mainheading>
       <Subheading>
-        These are all of private channels and peers in the network. You can click a private channel or a peer to
-        view more information and remove.
+        These are all of private channels in the network. You can click a private channel or a subscribed private channel
+        to view more information and remove.
       </Subheading>
       <Divider sx={{ marginY: 2 }} />
       <AddButton text="Create private channel" labelUrl="privateChannel"></AddButton>
@@ -207,11 +223,10 @@ export default function PrivateChannels() {
       <Subheading>My private channels</Subheading>
       <Divider style={{ margin: '5px 0', visibility: 'hidden' }} />
 
-      <TableContainer
-        component={Paper}
+      <Box
         sx={{
-          maxHeight: '400px',
-          overflowY: 'auto',
+          maxHeight: hasPeersData ? "400px": "hidden",
+          overflowY: hasPeersData ? "auto" : "hidden",
         }}
       >
         <DataGrid
@@ -233,37 +248,48 @@ export default function PrivateChannels() {
             handleDeletedItem={handleDeletedItem}
           />
         )}
-      </TableContainer>
-      <Divider style={{ margin: '20px 0', visibility: 'hidden' }} />
-      <Subheading>Private channels that I have subscribed to </Subheading>
-      <Divider style={{ margin: '5px 0', visibility: 'hidden' }} />
-
-      <TableContainer
-        component={Paper}
-        sx={{
-          maxHeight: '400px',
-          overflowY: 'auto',
-        }}
-      >
-        <DataGrid
-          columns={peerTableHeaders}
-          rows={peersData || []}
-          onRowClick={handleOnPeersRowClick}
-          loading={isPeersLoading}
-          getRowId={(row) => row.id}
-          sort={{ field: "lastUpdated", sort: "desc" }}
-          slots={{
-            noRowsOverlay: CustomEmptyOverlayPrivateChannels,
-          }}
+        <DeleteSubDialog
+          itemId={privateChannelRow?.id as string}
+          handleDialog={handleClickClose}
+          open={open}
+          actorCommonName={session?.user.commonName as string}
+          handleDeletedItem={handleDeletedItem}
+          text="Private channel"
         />
-        {peersRow && (
-          <PeersDrawer
-            handleMoreClose={handlePeersMoreClose}
-            open={peersDrawerOpen}
-            peers={peersRow as PrivateChannelPeers}
-          />
-        )}
-      </TableContainer>
+      </Box>
+
+      <Divider style={{ margin: '20px 0', visibility: 'hidden' }} />
+      {hasPeersData && (
+        <Box>
+          <Subheading>My subscribed private channels</Subheading>
+          <Divider style={{ margin: '5px 0', visibility: 'hidden' }} />
+          <Box
+            sx={{
+              maxHeight: hasPeersData ? "400px": "hidden",
+              overflowY: hasPeersData ? "auto" : "hidden",
+            }}
+          >
+            <DataGrid
+              columns={peerTableHeaders}
+              rows={peersData || []}
+              onRowClick={handleOnPeersRowClick}
+              loading={isPeersLoading}
+              getRowId={(row) => row.id}
+              sort={{ field: "lastUpdated", sort: "desc" }}
+              slots={{
+                noRowsOverlay: CustomEmptyOverlayPrivateChannels
+              }}
+            />
+            {peersRow && (
+              <PeersDrawer
+                handleMoreClose={handlePeersMoreClose}
+                open={peersDrawerOpen}
+                peers={peersRow as PrivateChannelPeers}
+              />
+            )}
+          </Box>
+        </Box>
+      )}
     </Box>
   );
 }
