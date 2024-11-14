0ef31b8aa5ab850ad840eb5d89b6929f9650046d
Maryam Mehrabian <marme660@users.noreply.github.com>
2024-11-14 15:15:12 +0100

    Added more tests for timeConverter

--- __tests__/lib/timeConverter.test.ts
+++ __tests__/lib/timeConverter.test.ts
@@ -2,17 +2,17 @@ import { timeConverter } from "@/lib/timeConverter";
 
 describe("Can correctly convert Unix Epoch", () => {
   const epoch = 1731527848;
-  const frenchDate = "13 nov. 2024 - 20:57:28";
-  const persianDate = "۲۳ آبان ۱۴۰۳ - ۲۰:۵۷:۲۸";
+  const englishDateFormat = "13 Nov 2024 - 20:57:28";
+  const persianDateFormat = "۲۳ آبان ۱۴۰۳ - ۲۰:۵۷:۲۸";
 
-  it("can convert to correct french date", () => {
-    jest.spyOn(navigator, 'language', 'get').mockReturnValue('fre-FR');
-    expect(timeConverter(epoch)).toBe(frenchDate);
+  it("can convert to correct american date and time format", () => {
+    jest.spyOn(navigator, 'language', 'get').mockReturnValue('en-GB');
+    expect(timeConverter(epoch)).toBe(englishDateFormat);
   });
 
-  it("can convert to correct persian date", () => {
+  it("can convert to correct persian date and time format", () => {
     jest.spyOn(navigator, 'language', 'get').mockReturnValue('fa-IR');
-    expect(timeConverter(epoch)).toBe(persianDate);
+    expect(timeConverter(epoch)).toBe(persianDateFormat);
   });
 
   afterEach(() => {
